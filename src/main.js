7475081f370b8d5fb0151b07e46dd526a149ad0e
sanjoyz <sanjoyz1105@gmail.com>
2019-08-07 10:39:48 +0300

    домашнее задание 3

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 'use strict';
-const getMenuTemplate = () => {
-  return `<section class="control__btn-wrap">
+const getMenuTemplate = () => (
+  `<section class="control__btn-wrap">
       <input
         type="radio"
         name="control"
@@ -27,19 +27,16 @@ const getMenuTemplate = () => {
       <label for="control__statistic" class="control__label"
         >STATISTICS</label
       >
-    </section>`;
-};
-const getSearchTemplate = () => {
-  return `<input
+    </section>`);
+const getSearchTemplate = () => (
+  `<input
      type="text"
      id="search__input"
      class="search__input"
      placeholder="START TYPING — SEARCH BY WORD, #HASHTAG OR DATE"
    />
-   <label class="visually-hidden" for="search__input">Search</label>`;
-};
-const getFilterTemplate = () => {
-  return `<input
+   <label class="visually-hidden" for="search__input">Search</label>`);
+const getFilterTemplate = () => (`<input
       type="radio"
       id="filter__all"
       class="filter__input visually-hidden"
@@ -104,10 +101,10 @@ const getFilterTemplate = () => {
     />
     <label for="filter__archive" class="filter__label"
       >Archive <span class="filter__archive-count">115</span></label
-    >`;
-};
-const getCardTemplate = () => {
-  return `<div class="card__form">
+    >`);
+const getCardTemplate = () => (
+  `<article class="card card--black">
+      <div class="card__form">
       <div class="card__inner">
         <div class="card__control">
           <button type="button" class="card__btn card__btn--edit">
@@ -169,10 +166,12 @@ const getCardTemplate = () => {
           </div>
         </div>
       </div>
-    </div>`;
-};
-const getCardEditTemplate = () => {
-  return `<form class="card__form" method="get">
+    </div>
+    </article>`
+);
+const getCardEditTemplate = () => (
+  `<article class="card card--edit card--black">
+  <form class="card__form" method="get">
       <div class="card__inner">
         <div class="card__control">
           <button type="button" class="card__btn card__btn--archive">
@@ -436,21 +435,20 @@ const getCardEditTemplate = () => {
           <button class="card__delete" type="button">delete</button>
         </div>
       </div>
-    </form>`;
-};
+    </form>
+    </article`);
 
 const getLoadMoreButtonTemplate = () => `<button class="load-more" type="button">load more</button>`;
 
 const renderComponents = (container, template) => {
-  const containerElement = document.createElement(`${container}`);
-  document.querySelector(`.main`).appendChild(containerElement);
-  containerElement.insertAdjacentHTML(`afterbegin`, template);
+  const containerElement = document.querySelector(`${container}`);
+  containerElement.insertAdjacentHTML(`beforeend`, template);
 };
-renderComponents(`article`, getCardEditTemplate());
-renderComponents(`section`, getMenuTemplate());
-renderComponents(`section`, getSearchTemplate());
-renderComponents(`section`, getFilterTemplate());
-renderComponents(`section`, getLoadMoreButtonTemplate());
-renderComponents(`article`, getCardTemplate());
-renderComponents(`article`, getCardTemplate());
-renderComponents(`article`, getCardTemplate());
+renderComponents(`.board__tasks`, getCardEditTemplate());
+renderComponents(`.main__control`, getMenuTemplate());
+renderComponents(`.main__search`, getSearchTemplate());
+renderComponents(`.main__filter`, getFilterTemplate());
+renderComponents(`.board.container`, getLoadMoreButtonTemplate());
+for (let i = 0; i < 3; i++) {
+  renderComponents(`.board__tasks`, getCardTemplate());
+}
